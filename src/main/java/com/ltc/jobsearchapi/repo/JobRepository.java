1b81d998d60151ad952591c2cb8ba5ca0248dced
Ali <aaliyev14291@ada.edu.az>
2026-04-22 14:12:58 +0400

    JobRepo Query

---
+++ src/main/java/com/ltc/jobsearchapi/repo/JobRepository.java
@@ -0,0 +1,33 @@
+package com.ltc.jobsearchapi.repo;
+
+import com.ltc.jobsearchapi.model.Job;
+import com.ltc.jobsearchapi.model.JobType;
+import org.springframework.data.domain.Page;
+import org.springframework.data.domain.Pageable;
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.jpa.repository.Query;
+import org.springframework.stereotype.Repository;
+import org.springframework.data.repository.query.Param;
+
+@Repository
+public interface JobRepository extends JpaRepository<Job, Long> {
+
+    @Query("""
+        SELECT j FROM Job j
+        WHERE 
+        (:keyword IS NULL OR 
+            LOWER(j.title) LIKE LOWER(CONCAT('%', :keyword, '%')) OR
+            LOWER(j.description) LIKE LOWER(CONCAT('%', :keyword, '%'))
+        )
+        AND (:location IS NULL OR LOWER(j.location) = LOWER(:location))
+        AND (:jobType IS NULL OR j.jobType = :jobType)
+    """)    // use lower for case-insensitive
+
+    Page<Job> searchJobs(
+            // use parameter to prevent injection
+            @Param("keyword") String keyword,
+            @Param("location") String location,
+            @Param("jobType") JobType jobType,
+            Pageable pageable
+    );
+}
