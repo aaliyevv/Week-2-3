3b6e7433332fb2ee91678a11192204464dbeba75
Ali <aaliyev14291@ada.edu.az>
2026-04-23 16:08:29 +0400

    Error Response dto

---
+++ src/main/java/com/ltc/jobsearchapi/dto/ErrorResponse.java
@@ -0,0 +1,16 @@
+package com.ltc.jobsearchapi.dto;
+
+import lombok.Builder;
+import lombok.Data;
+
+import java.time.LocalDateTime;
+
+@Data
+@Builder
+public class ErrorResponse {
+
+    private String message;
+    private int status;
+    private LocalDateTime timestamp;
+
+}
--- src/main/java/com/ltc/jobsearchapi/exception/BadRequestException.java
+++ src/main/java/com/ltc/jobsearchapi/exception/BadRequestException.java
@@ -2,6 +2,7 @@ package com.ltc.jobsearchapi.exception;
 
 public class BadRequestException extends RuntimeException {
     public BadRequestException(String message) {
+
         super(message);
     }
 }
